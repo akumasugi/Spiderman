01f61cf2b8feff34eab11cfac4278c14e3d59e05
Parkerrr <96352539+ProSpotGamer@users.noreply.github.com>
2022-04-28 09:20:50 +0530

    Update Chitoge.ts

--- src/commands/General/Chitoge.ts
+++ src/commands/General/Chitoge.ts
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `🐧 *Miles Morales* 🐧\n\n🍀 *Description: A WhatsApp Bot created by PARKER With Rich Anime Features.*\n\n🌐 *URL: https://github.com/ProSpotGamer/Miles-Bot.git* \n\n 📒 *Guide: https://github.com/ProSpotGamer/Miles-Bot.git* \n`,
+				caption: `😎 *Miles Morales* 😎\n\n🍀 *Description: A WhatsApp Bot created by PARKER With Rich Anime Features.*\n\n🌐 *URL: https://github.com/ProSpotGamer/Miles-Bot.git* \n\n 📒 *Guide: https://github.com/ProSpotGamer/Miles-Bot.git* \n`,
 			}
 		);
 	};
