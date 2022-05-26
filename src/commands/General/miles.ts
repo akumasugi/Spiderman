ca691fb2470397e7b99ef94fb2d6f0cb48949863
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-26 11:50:59 +0530

    Create miles.ts

---
+++ src/commands/General/miles.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "miles",
+			description: "Displays the info",
+			category: "general",
+			usage: `${client.config.prefix}chitoge`,
+			baseXp: 200,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://c.tenor.com/1ZjBNXcYEXkAAAAC/miles-morales.gif";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `🕷️ *Miles Morales* 🕷️\n\n🕷️ *Description: repo is private better luck next time.*\n\n🕷️ *insta id: https://www.instagram.com/akuma__24/?hl=en* \n\n 🕷️\n`,
+			}
+		);
+	};
+}
