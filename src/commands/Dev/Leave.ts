375d3e180741872b519cd5d64289ef17881c7dd9
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-26 10:55:27 +0530

    Update Leave.ts

--- src/commands/Dev/Leave.ts
+++ src/commands/Dev/Leave.ts
@@ -17,7 +17,7 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        await M.reply(`*Goodbye* 👋`)
+        await M.reply(`*Goodbye* 🕷️`)
         await this.client.groupLeave(M.from).catch(() => M.reply('Failed to leave the Group'))
     }
 }
