0d2d90425b0b09babc6ea719b8aeb70612ad1aa3
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-05-26 10:49:27 +0530

    Update Rank.ts

--- src/commands/General/Rank.ts
+++ src/commands/General/Rank.ts
@@ -32,9 +32,9 @@ export default class Command extends BaseCommand {
 		try {
 			pfp = await this.client.getProfilePicture(user);
 		} catch (err) {
-			M.reply(`Profile Picture not Accessible of ${username}`);
+			M.reply(`Profile Picture not Accessible of ${username} must be ugly probably`);
 			pfp =
-				"https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg";
+				"https://i.ibb.co/2sLT465/Homem-Aranha-Spinoff-El-Muerto-anunciado-pela-Sony.jpg";
 		}
 		const exp = (await this.client.getUser(user)).Xp;
 		let role: string;
